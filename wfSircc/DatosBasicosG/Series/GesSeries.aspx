a16df1e34948c4999ae1d6eb76bf2eaa2ee69f50
kevinhmendoza@gmail.com <kevinhmendoza@gmail.com@1d1c00c0-8e48-6dec-74bc-90d65bc2d916>
2014-09-01 18:45:01 +0000

    formulario para la consulta y edicion de Series..

---
+++ wfSircc/DatosBasicosG/Series/GesSeries.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteBS.Master" AutoEventWireup="true" CodeBehind="GesSeries.aspx.cs" Inherits="wfSircc.DatosBasicosG.Series.GesSeries" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div class="row">
+            <div class="col-md-12">               
+                <div class="col-md-6">
+                        <div class="btn-toolbar">
+                        <button type="button" class="btn btn-warning" id="nuevoButton">
+                            <span class="glyphicon glyphicon-plus-sign"></span>
+                            Nuevo</button>                          
+                        <button type="button" class="btn btn-primary" id="editarButton">
+                            <span class="glyphicon glyphicon-pencil"></span>
+                            Editar</button>                  
+                                   
+                    </div>
+                </div>    
+               
+            </div>           
+        </div>
+       <br />
+    <br />
+      <div class="row">                      
+                      <div class="col-md-3">  
+                                         Listado de Series  
+                      </div>                                      
+                      <div class="col-md-12">  
+                             &nbsp                            
+                      </div>                                     
+                      <div class="col-md-12"> 
+                      <div id="jqxgridHisto"></div> 
+                      </div>
+         </div>   
+     <script src="js/GesSeries.js"></script>
+</asp:Content>
---
+++ wfSircc/DatosBasicosG/Series/js/GesSeries.js
@@ -0,0 +1,103 @@
+﻿var SeriesList = (function () {
+    "use strict";
+    var grid = '#jqxgridHisto';
+    var urlToGridSeries = "/Servicios/Archivos/wsSeries.asmx/GetSeries";
+    var byaRpta;
+    var msgPpal = "#LbMsg";
+    var urlToNuevo="Series.aspx"
+    var _addHandlers = function () {
+        $("#nuevoButton").click(function () {
+            byaPage.AbrirPagina(urlToNuevo);
+        });
+        $("#editarButton").click(function () {
+
+            var dataRecord = SeriesList.getRecord();
+            if (dataRecord != undefined) {
+                var target = urlToNuevo + "?idSerie=" + dataRecord.idSerie;
+                byaPage.AbrirPagina(target);
+            } else { alert("Debe Selecionar una fila de la tabla");}
+        });
+
+
+    };
+    var _createElements = function () {
+        _createGrid();
+    };
+    var getDataAdapter = function () {
+        var source = {
+
+            datatype: "xml",
+            datafields: [
+	                { name: 'idSerie', type: "String" },
+                    { name: 'Serie' },
+                    { name: 'Procedimiento' }
+            ],
+            async: true,
+            record: 'Table',
+            url: urlToGridSeries,
+            data: {}
+        };
+        var dataAdapter = new $.jqx.dataAdapter(source, { contentType: 'application/json; charset=utf-8' });
+        return dataAdapter;
+    };
+    var _createGrid = function () {
+
+        $(grid).jqxGrid(
+            {
+                width: '100%',
+                source: getDataAdapter(),
+                theme: SeriesList.config.theme,
+                altrows: true,
+                editable: false,
+                sortable: true,
+                showfilterrow: true,
+                autoheight: true,
+                autorowheight: true,
+                filterable: true,
+                pageable: true,
+                enabletooltips: true,
+                localization: byaPage.getLocalization(),
+                columns: [
+                  { text: 'Id Serie     ', datafield: 'idSerie' },
+                  { text: 'Nombre Serie ', datafield: 'Serie' },
+                  { text: 'Procedimiento Cesion', datafield: 'Procedimiento' }
+
+                ]
+            });
+
+    };
+    return {
+        id_ep: null,
+        fnresultado: null,
+        editedRows: null,
+        config: {
+            dragArea: null,
+            theme: null
+        },
+        Vigencia: function () {
+            return byaSite.getVigencia();
+        },
+        getRecord: function () {          
+            var selectedrowindex = $(grid).jqxGrid('getselectedrowindex');
+            var dataRecord = $(grid).jqxGrid('getrowdata', selectedrowindex);
+            return dataRecord;
+        },
+        refresh: function () {
+            $(grid).jqxGrid({ source: getDataAdapter() });
+        },
+        init: function () {
+            _addHandlers();
+            _createElements();
+
+
+        }
+    };
+
+}());
+
+$(function () {
+    byaSite.SetModuloP({ TituloForm: "Consulta de Series", Modulo: "", urlToPanelModulo: "#", Cod_Mod: "", Rol: "" });   
+    SeriesList.config.theme = byaSite.tema
+    SeriesList.init();
+
+});
