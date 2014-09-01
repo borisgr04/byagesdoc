efffabb6a03b15e61ee5d588726899357829b3f2
kevinhmendoza@gmail.com <kevinhmendoza@gmail.com@1d1c00c0-8e48-6dec-74bc-90d65bc2d916>
2014-09-01 19:26:24 +0000

    formulario para la consulta de SubSeries

---
+++ wfSircc/DatosBasicosG/SubSeries/GesSubSeries.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteBS.Master" AutoEventWireup="true" CodeBehind="GesSubSeries.aspx.cs" Inherits="wfSircc.DatosBasicosG.SubSeries.GesSubSeries" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+            <div class="col-md-12">               
+                <div class="col-md-6">
+                        <div class="btn-toolbar">
+                        <button type="button" class="btn btn-warning" id="nuevoButton">
+                            <span class="glyphicon glyphicon-plus-sign"></span>
+                            Nuevo</button>                          
+                        <button type="button" class="btn btn-primary" id="editarButton">
+                            <span class="glyphicon glyphicon-pencil"></span>
+                            Editar</button>   
+                            <button type="button" class="btn btn-danger" id="anularButton">
+                            <span class="glyphicon glyphicon-remove"></span>
+                            Anular</button>                 
+                                   
+                    </div>
+                </div>    
+               
+            </div>           
+        </div>
+       <br />
+    <br />
+      <div class="row">                      
+                      <div class="col-md-3">  
+                                         Listado de SubSeries  
+                      </div>                                      
+                      <div class="col-md-12">  
+                             &nbsp                            
+                      </div>                                     
+                      <div class="col-md-12"> 
+                      <div id="jqxgridHisto"></div> 
+                      </div>
+         </div>   
+     <script src="js/GesSubSeries.js"></script>
+
+</asp:Content>
---
+++ wfSircc/DatosBasicosG/SubSeries/js/GesSubSeries.js
@@ -0,0 +1,141 @@
+﻿var SubSeriesList = (function () {
+    "use strict";
+    var grid = '#jqxgridHisto';
+    var urlToGrid = "/Servicios/Archivos/wsSubSeries.asmx/Gets";
+    var urlToAnular = "/Servicios/Archivos/wsSubSeries.asmx/Anular";
+    var byaRpta;
+    var msgPpal = "#LbMsg";
+    var urlToNuevo = "SubSeries.aspx"
+    var _addHandlers = function () {
+        $("#nuevoButton").click(function () {
+            byaPage.AbrirPagina(urlToNuevo);
+        });
+        $("#editarButton").click(function () {
+
+            var dataRecord = SubSeriesList.getRecord();
+            if (dataRecord != undefined) {
+                var target = urlToNuevo + "?idSubSerie=" + dataRecord.idSubSerie;
+                byaPage.AbrirPagina(target);
+            } else { alert("Debe Selecionar una fila de la tabla"); }
+        });
+        $("#anularButton").click(function () {
+
+            Anular();
+        });
+
+
+    };
+    var Anular = function () {
+        var dataRecord = SubSeriesList.getRecord();
+        if (dataRecord == undefined) {
+            alert("Debe Selecionar Una Fila");
+        }
+        else {
+            var jsonData = "{'Reg':" + JSON.stringify(dataRecord) + "}";
+            byaPage.POST_Sync(urlToAnular, jsonData, function (result) {
+                byaRpta = byaPage.retObj(result.d);
+                SubSeriesList.refresh();
+                $(msgPpal).msgBox({ titulo: "Anulación de SubSeries", mensaje: byaRpta.Mensaje, tipo: !byaRpta.Error });
+                if (!byaRpta.Error) {
+
+                }
+
+            });
+        }
+    }
+    var _createElements = function () {
+        _createGrid();
+    };
+    var getDataAdapter = function () {
+        var source = {
+          
+      
+            datatype: "xml",
+            datafields: [
+	                { name: 'idSubSeries', type: "String" },
+                    { name: 'SubSerie' },
+                    { name: 'RetencionAG' },
+                    { name: 'DisposicionA' },
+                    { name: 'DisposicionCT' },
+                    { name: 'DisposicionE' },
+                    { name: 'DisposicionMD' },
+                    { name: 'DisposicionS' },
+                    { name: 'Series_idSerie' }
+            ],
+            async: true,
+            record: 'Table',
+            url: urlToGrid,
+            data: {}
+        };
+        var dataAdapter = new $.jqx.dataAdapter(source, { contentType: 'application/json; charset=utf-8' });
+        return dataAdapter;
+    };
+    var _createGrid = function () {
+
+        $(grid).jqxGrid(
+            {
+                width: '100%',
+                source: getDataAdapter(),
+                theme: SubSeriesList.config.theme,
+                altrows: true,
+                editable: false,
+                sortable: true,
+                showfilterrow: true,
+                autoheight: true,
+                autorowheight: true,
+                filterable: true,
+                pageable: true,
+                enabletooltips: true,
+                localization: byaPage.getLocalization(),
+                columns: [
+                  { text: 'Id', datafield: 'idSubSeries', width: 150 },
+                  { text: 'Nombre Serie ', datafield: 'SubSerie', width: 150 },
+                  { text: 'RetencionAG', datafield: 'RetencionAG', width: 150 },
+                  { text: 'DisposicionA', datafield: 'DisposicionA', width: 150 },
+                  { text: 'DisposicionCT', datafield: 'DisposicionCT', width: 150 },
+                  { text: 'DisposicionE', datafield: 'DisposicionE', width: 150 },
+                  { text: 'DisposicionMD', datafield: 'DisposicionMD', width: 150 },
+                  { text: 'DisposicionS', datafield: 'DisposicionS', width: 150 },
+                  { text: 'Series_idSerie ', datafield: 'Series_idSerie', width: 150 },
+               
+              
+
+                ]
+            });
+
+    };
+    return {
+        id_ep: null,
+        fnresultado: null,
+        editedRows: null,
+        config: {
+            dragArea: null,
+            theme: null
+        },
+        Vigencia: function () {
+            return byaSite.getVigencia();
+        },
+        getRecord: function () {
+            var selectedrowindex = $(grid).jqxGrid('getselectedrowindex');
+            var dataRecord = $(grid).jqxGrid('getrowdata', selectedrowindex);
+            return dataRecord;
+        },
+        refresh: function () {
+            $(grid).jqxGrid({ source: getDataAdapter() });
+        },
+        init: function () {
+            _addHandlers();
+            _createElements();
+
+
+        }
+    };
+
+}());
+
+$(function () {
+    byaSite.SetModuloP({ TituloForm: "Consulta de SubSeries", Modulo: "", urlToPanelModulo: "#", Cod_Mod: "", Rol: "" });
+    SubSeriesList.config.theme = byaSite.tema
+    SubSeriesList.init();
+
+});
