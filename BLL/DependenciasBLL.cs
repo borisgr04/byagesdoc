4e649d1539b851f1867d69d934d73bfc2e3a94ed
kevinhmendoza@gmail.com <kevinhmendoza@gmail.com@1d1c00c0-8e48-6dec-74bc-90d65bc2d916>
2014-09-02 21:28:29 +0000

    se agregaron los metodos insert, update y anular

--- BLL/DependenciasBLL.cs
+++ BLL/DependenciasBLL.cs
@@ -1,4 +1,5 @@
 ﻿using BLL.Gestion;
+using ByA;
 using Entidades;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,18 @@ namespace BLL
     public class DependenciasBLL
     {
         mDependencias DepManager = new mDependencias();
-        //public ByARpt Insert( Reg)
-        //{
-        //    return SManager.Insert(Reg);
-        //}
+        public ByARpt Insert(dependenciasDto Reg)
+        {
+            return DepManager.Insert(Reg);
+        }
+        public ByARpt Update(dependenciasDto Reg)
+        {
+            return DepManager.Update(Reg);
+        }
+        public ByARpt Anular(dependenciasDto Reg)
+        {
+            return DepManager.Anular(Reg);
+        }
         public List<dependenciasDto> Gets()
         {
             return DepManager.Gets();
