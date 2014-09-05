6159bb70e7815252a581c05f0c26e1bdb3534b1e
kevinhmendoza@gmail.com <kevinhmendoza@gmail.com@1d1c00c0-8e48-6dec-74bc-90d65bc2d916>
2014-09-05 03:48:35 +0000

    clases para la gestion de relacion documental TipoDocumental vs Unida Documental

---
+++ BLL/Gestion/mUnidadD_TipoDoc.cs
@@ -0,0 +1,153 @@
+﻿using AutoMapper;
+using ByA;
+using DAL;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Gestion
+{
+    public class mUnidadD_TipoDoc:absBLL
+    {
+
+        public mUnidadD_TipoDoc()
+         {
+
+             Mapper.CreateMap<UnidadD_TipoDocDto, unidadd_tipodoc>();
+             Mapper.CreateMap<unidadd_tipodoc, UnidadD_TipoDocDto>();
+         }
+
+        public ByARpt Insert(UnidadD_TipoDocDto Reg)
+         {
+             unidadd_tipodoc r = new unidadd_tipodoc();
+             Mapper.Map(Reg, r);
+             cmdInsert o = new cmdInsert { reg = r };
+             return o.Enviar();
+         }
+        public ByARpt Update(UnidadD_TipoDocDto Reg)
+         {
+             unidadd_tipodoc r = new unidadd_tipodoc();
+             Mapper.Map(Reg, r);
+             cmdUpdate o = new cmdUpdate { reg = r };
+             return o.Enviar();
+         }
+        public ByARpt Anular(UnidadD_TipoDocDto Reg)
+         {
+             unidadd_tipodoc r = new unidadd_tipodoc();
+             Mapper.Map(Reg, r);
+             cmdAnular o = new cmdAnular { reg = r };
+             return o.Enviar();
+         }
+
+        public List<UnidadD_TipoDocDto> Gets()
+         {
+             List<UnidadD_TipoDocDto> lstT = new List<UnidadD_TipoDocDto>();
+             using (ctx = new trdEntities())
+          {
+              List<unidadd_tipodoc> lstO = ctx.unidadd_tipodoc.ToList();
+              Mapper.Map(lstO, lstT);
+          }
+          return lstT;
+      }
+        public UnidadD_TipoDocDto Get(string Id)
+      {
+          UnidadD_TipoDocDto objT = new UnidadD_TipoDocDto();
+          using (ctx = new trdEntities())
+          {
+              unidadd_tipodoc objO = ctx.unidadd_tipodoc.Find(Id);
+              Mapper.Map(objO, objT);
+          }
+          return objT;
+      }
+
+         class cmdInsert : absTemplate
+         {
+
+             public unidadd_tipodoc found { get; set; }
+             public unidadd_tipodoc reg { get; set; }
+             protected internal override bool esValido()
+             {
+                 return true;
+             }
+             protected internal override void Antes()
+             {
+
+                 ctx.unidadd_tipodoc.Add(reg);
+
+             }
+
+
+         }
+         class cmdUpdate : absTemplate
+         {
+
+             public unidadd_tipodoc reg { get; set; }
+             public unidadd_tipodoc found { get; set; }
+             protected internal override bool esValido()
+             {
+                 found = ctx.unidadd_tipodoc.Where(t => t.IdTipoDoc == reg.IdTipoDoc && t.IdUnidadDoc==reg.IdUnidadDoc).FirstOrDefault();
+                 if (found != null)
+                 {
+                     found.Codigo = reg.Codigo;
+                     found.Descripcion = reg.Descripcion;
+                     found.FechaDoc = reg.FechaDoc;
+                     found.CantidadPag = reg.CantidadPag;
+                     found.PagIni = reg.PagIni;                 
+                   
+                   
+                
+                     return true;
+                 }
+                 else
+                 {
+                     byaRpt.Mensaje = "No se encontro El TipoDocumental o la Unidad Documental";
+                     byaRpt.Error = true;
+                     return !byaRpt.Error;
+                 }
+
+
+             }
+             protected internal override void Antes()
+             {
+                
+                 ctx.Entry(found).State = System.Data.Entity.EntityState.Modified; 
+             }
+
+
+         }
+         class cmdAnular : absTemplate
+         {
+
+             public unidadd_tipodoc found { get; set; }
+             public unidadd_tipodoc reg { get; set; }
+             protected internal override bool esValido()
+             {
+                 found = ctx.unidadd_tipodoc.Where(t => t.IdTipoDoc == reg.IdTipoDoc && t.IdUnidadDoc == reg.IdUnidadDoc).FirstOrDefault();
+                 if (found != null)
+                 {
+                     //found.Estado = "AN";
+                     return true;
+                 }
+                 else
+                 {
+                     byaRpt.Mensaje = "No se encontro El TipoDocumental o la Unidad Documental";
+                     byaRpt.Error = true;
+                     return !byaRpt.Error;
+                 }
+
+
+             }
+             protected internal override void Antes()
+             {
+                
+                 ctx.Entry(found).State = System.Data.Entity.EntityState.Modified; 
+             }
+
+
+         }
+
+
+    }
+}
---
+++ BLL/UnidadD_TipoDocBLL.cs
@@ -0,0 +1,36 @@
+﻿using BLL.Gestion;
+using ByA;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+   public  class UnidadD_TipoDocBLL
+    {
+       mUnidadD_TipoDoc UniManager = new mUnidadD_TipoDoc();
+       public ByARpt Insert(UnidadD_TipoDocDto Reg)
+        {
+            return UniManager.Insert(Reg);
+        }
+        public ByARpt Update(UnidadD_TipoDocDto Reg)
+        {
+            return UniManager.Update(Reg);
+        }
+        public ByARpt Anular(UnidadD_TipoDocDto Reg)
+        {
+            return UniManager.Anular(Reg);
+        }
+        public List<UnidadD_TipoDocDto> Gets()
+        {
+            return UniManager.Gets();
+        }
+        public UnidadD_TipoDocDto Get(string idUnidad)
+        {
+            return UniManager.Get(idUnidad);
+        }
+
+    }
+}
