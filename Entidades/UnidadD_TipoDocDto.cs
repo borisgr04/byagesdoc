20515c6ddb674e2cf6ec7dcd7c00109c6e5a5b84
kevinhmendoza@gmail.com <kevinhmendoza@gmail.com@1d1c00c0-8e48-6dec-74bc-90d65bc2d916>
2014-09-05 03:49:29 +0000

    se agrego la clase Dto para la gestion de relacion documental

---
+++ Entidades/UnidadD_TipoDocDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entidades
+{
+    public class UnidadD_TipoDocDto
+    {
+        public string IdUnidadDoc { get; set; }
+        public string IdTipoDoc { get; set; }
+        public string Codigo { get; set; }
+        public string FechaDoc { get; set; }
+        public string PagIni { get; set; }
+        public string CantidadPag { get; set; }
+        public string Descripcion { get; set; }
+
+        //public virtual unidaddocumental unidaddocumental { get; set; }
+        //public virtual tiposdocumentales tiposdocumentales { get; set; }
+    }
+}
