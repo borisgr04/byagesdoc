2e54716f320791a53c603b82f86f229d91f6ac5b
borisgr04@gmail.com <borisgr04@gmail.com>
2014-09-11 12:07:26 +0000

    se actualiza modelo.

--- DAL/Model1.Designer.cs
+++ DAL/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'E:\ByA\2014\Dev\GDoc\DAL\Model1.edmx'. 
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'D:\Programas\GesDoc\DAL\Model1.edmx'. 
 // Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
